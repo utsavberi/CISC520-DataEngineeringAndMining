092d8a6635e57bf10d7de67e79b7a33d25e62a39
uberi <uberi@wayfair.com>
2020-04-02 01:20:09 -0400

    cluster analysis

--- clusterAnalysis.py
+++ clusterAnalysis.py
@@ -9,25 +9,26 @@ from dataCleaning import cleanData
 
 
 df_train = pd.read_csv('data/ICPSR_35509/DS0001/35509-0001-Data.tsv', delimiter='\t', encoding='utf-8') # shape (55160, 3141)
-df_train_columns = np.array(df_train.columns).T
 # df_train = pd.read_csv('data_tail_5000.csv')
 df_train = cleanData(df_train)
-i = 0
-for col in df_train.columns:
-    print(i)
-    print(col)
-    i = i+1
+df_train_columns = np.array(df_train.columns)
+print(df_train.shape)
+print(df_train_columns)
+# i = 0
+# for col in df_train.columns:
+#     print(i)
+#     print(col)
+#     i = i+1
 df_train = StandardScaler().fit_transform(df_train)
-
+# print(df_train.columns)
 clusters = np.genfromtxt('pca_data_no_missing_full_clusters', delimiter=',')
 # clusters = clusters[-5000:]
 print(clusters.shape)
-print(df_train.shape)
 # df_train['clusters'] = clusters
 # print(df_train.shape)
-print(df_train[0, :])
-print(clusters)
-print(clusters.T)
+# print(df_train[0, :])
+# print(clusters)
+# print(clusters.T)
 results = []
 
 for column in df_train.T:
@@ -39,48 +40,56 @@ for column in df_train.T:
     # print(column)
 
 results = np.array(results)
-print(results.shape)
+
+# np.savetxt('column_means_w_cluster_id', np.hstack((df_train_columns, results.shape)))
 
 labels = np.arange(0, results.shape[0])
-print(labels.shape)
+# print(labels.shape)
 cluster1_means = results[:, 0]
 cluster2_means = results[:, 1]
-np_array = np.array(np.hstack((df_train_columns, results)))
-np.savetxt('gaussian_cluster_analysis_no_missing_data.csv', X=np_array, delimiter=',')
-print(cluster1_means.shape)
-print(cluster2_means.shape)
+# np.savetxt('cluster1_column_means', X=cluster1_means)
+# np.savetxt('cluster2_column_means', X=cluster2_means)
+# print(results.shape)
+# print(df_train_columns.shape)
+# np.savetxt('column names.csv', X=np.array(df_train_columns, dtype='string'), delimiter=',')
+# np.savetxt('gaussian_cluster_analysis_no_missing_data.csv', X=np_array, delimiter=',')
+# np_array = np.array(np.hstack((np.array(df_train_columns), results)))
+# np.savetxt('gaussian_cluster_analysis_no_missing_data_w_columns.csv', X=np_array, delimiter=',', fmt="%s")
 
-#
-#
-# width = 0.3  # the width of the bars
-#
-# fig, ax = plt.subplots()
-# rects1 = ax.bar(labels - width/2, cluster1_means, width, label='Cluster1')
-# rects2 = ax.bar(labels + width/2, cluster2_means, width, label='Cluster2')
-#
-# # Add some text for labels, title and custom x-axis tick labels, etc.
-# ax.set_ylabel('Scores')
-# ax.set_title('Scores by group and gender')
-# # ax.set_xticks(labels)
-# # ax.set_xticklabels(labels)
-# ax.legend()
-#
-#
-# def autolabel(rects):
-#     """Attach a text label above each bar in *rects*, displaying its height."""
-#     for rect in rects:
-#         height = rect.get_height()
-#         ax.annotate('{}'.format(height),
-#                     xy=(rect.get_x() + rect.get_width() / 2, height),
-#                     xytext=(0, 3),  # 3 points vertical offset
-#                     textcoords="offset points",
-#                     ha='center', va='bottom')
-#
-#
-# # autolabel(rects1)
-# # autolabel(rects2)
-#
-# fig.tight_layout()
-# print('showing')
-# plt.show()
+# print(cluster1_means.shape)
+# print(cluster2_means.shape)
+
+
+
+width = 1  # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(labels - width/2, cluster1_means, width, label='Cluster1')
+rects2 = ax.bar(labels + width/2, cluster2_means, width, label='Cluster2')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Scores')
+ax.set_title('Scores by group and gender')
+# ax.set_xticks(labels)
+# ax.set_xticklabels(labels)
+ax.legend()
+
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
+# autolabel(rects1)
+# autolabel(rects2)
+
+fig.tight_layout()
+print('showing')
+plt.show()
 print('done')
--- dataCleaning.py
+++ dataCleaning.py
@@ -19,7 +19,21 @@ def cleanData(arrdf):
 
 #
 df_train = pd.read_csv('data_tail_5000.csv')
-print(df_train)
-print(df_train.columns)
+np.savetxt('column_names.csv',X=np.array(df_train.columns), fmt="%s");
+# print(df_train)
+# print(df_train.columns.shape)
+# a = np.arange(0,3142*2)
+# a=np.reshape(a,(3142,-1))
+# print(a.shape)
+# print(df_train.columns)
+# b = np.array(df_train.columns)
+# print(type(b))
+# print(b.shape)
+# print(b[:,None].shape)
+# print(b.T.shape)
+# print(np.hstack((a, df_train.columns[:,None])))
 # df_train=df_train.drop(['Unnamed: 0', 'CASEID','QUESTID2'], axis=1)
 # print(df_train)
+# df_train = pd.read_csv('data_tail_5000.csv')
+# print(df_train.columns.shape)
+# print(np.array(df_train.columns).shape)
